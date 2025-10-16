9a9968ec78f9e634e3a81a78d018c07ee6e00ae2
Frost Leo <FrostLeo.Dev@gmail.com>
2025-10-16 10:24:16 +0800

    gh-3 [feat]: 2025-10-16 | Initialize Leviftas SDK package structure
    - Create src/leviftas/__init__.py with package initialization
    - Add version variable for future dynamic configuration
    - Include proper GPL-3.0 license headers and documentation
    - Set up basic package structure for Leviftas SDK
    
    Refs #3
    
    Signed-off-by: FrostLeo <frostleo.dev@gmail.com>

---
+++ src/leviftas/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Copyright © 2025 Leviftas authors. All rights reserved.
+
+Licensed under the GNU General Public License v3.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://www.gnu.org/licenses/gpl-3.0.html
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+__init__.py
+
+Leviftas SDK package initialization.
+
+- Author   : FrostLeo <frostleo.dev@gmail.com>
+- Created  : 2025/10/16 
+- Modified : 2025/10/16
+"""
+
+__author__: str = "Frost Leo"
+__email__: str = "frostleo.dev@gmail.com"
+__license__: str = "GPL-3.0-or-later"
+
+__version__: str = "0.1.0"
+
+__all__ = [
+    "__version__",
+]
